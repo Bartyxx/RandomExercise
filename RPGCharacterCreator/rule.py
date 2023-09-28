ba4e76ccb100a28b739691ee2b89508dc109f499
Bartyxx <145768376+Bartyxx@users.noreply.github.com>
2023-09-28 15:45:32 +0200

    Add files via upload

---
+++ RPGCharacterCreator/rule.py
@@ -0,0 +1,49 @@
+"""
+
+The users got ask gender and type of the character: 
+    
+Gender ---> can be male, female or other.
+            - Male -> more strong
+            - Female -> more dexterity
+            - Other -> more intelligent
+Class ----> if the users prefer a strong, agile or intelligent character
+            - Strong -> Warrior
+            - Agile -> Ranger
+            - Intelligence -> Mage
+
+Strenght -> Defined by the strenght point, male - strenght receive bonus.
+Dexterity -> Define by the agility point, female - dexterity receive bonus.
+Intellligence -> Define by the intelligencer poit, other - mage receive bonus.
+
+Extra abilities ---> They are 3:
+                     - Strenght ability
+                     - Agility ability
+                     - Intelligence ability
+                     The extra ability is activated when the poi for the class
+                     is => 70.
+
+==============================================================================
+
+The max for every indicator is 100, after the 50 point for a special ability 
+is unlocked. Every indicator begin with 0 for the creation of the character 
+and the point are assigned as follows:
+
+Strenght -> + 15 point strenght 
+Dexterity -> + 15 point dexterity
+Intelligence -> + 15 point intelligence
+
+Male -> + 15 point strenght
+Female -> + 15 point dexterity
+Other -> + 15 point intelligence
+
+A random number between 0 ad 45 is created for every indicaator, the sum of the
+previosu choice and the random number define the character.
+
+There are three bonus:
+Male + Strenght 
+Female + Agility
+Other + Intelligence
+In this three case the random number si generatedf between 10 and 55. 
+
+
+"""
